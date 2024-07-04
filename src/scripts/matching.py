0884c6d6f5515245a66eeea70be0d8980c69b7ee
James Petley <jwpetley@gmail.com>
2024-07-04 11:55:55 +0200

    Require correct script

--- src/scripts/matching.py
+++ src/scripts/matching.py
@@ -126,47 +126,6 @@ if __name__ == "__main__":
 
     matched_ai = z_six_micron_match(bals, ai_bals)
 
-    print(len(matched), len(bals), len(qsos))
-
-    plt.figure()
-
-    plt.scatter(bals["Z"], bals["six_micron"], label="BI BALs", s=1)
-    plt.scatter(qsos["Z"], qsos["six_micron"], label="QSOs", s=1)
-    plt.scatter(matched["Z"], matched["six_micron"], label="Matched QSOs", s=1)
-
-    plt.xlabel(r"$z$")
-    plt.ylabel(r"$\log_{10} L_{6 \mu m}$")
-    plt.legend()
-    plt.close()
-
-    #Same as a kde plot
-    fig, axs = plt.subplots(2, 2, sharex=True, sharey=True)
-    axs = axs.flatten()
-    
-    axs[0].hist2d(bals["Z"], bals["six_micron"], bins=50, cmap="viridis")
-
-    axs[1].hist2d(qsos["Z"], qsos["six_micron"], bins=50, cmap="viridis")
-
-    axs[2].hist2d(matched["Z"], matched["six_micron"], bins=50, cmap="viridis")
-
-    axs[3].hist2d(matched_ai["Z"], matched_ai["six_micron"], bins=50, cmap="viridis")
-
-    axs[0].set_xlabel(r"$z$")
-    axs[0].set_ylabel(r"$\log_{10} L_{6 \mu m}$")
-    axs[1].set_xlabel(r"$z$")
-    axs[2].set_xlabel(r"$z$")
-    axs[1].set_ylabel(r"$\log_{10} L_{6 \mu m}$")
-    axs[0].set_title("BI BALs")
-    axs[1].set_title("QSOs")
-    axs[2].set_title("Matched QSOs")
-
-    plt.tight_layout()
-
-    plt.savefig(paths.figures / "six_micron_z.pdf", bbox_inches = "tight",
-                dpi = 300)
-
-    plot_detection_fraction(bals, matched_ai, matched, "matched_detection_fraction.pdf")
-
     matched.write(paths.data / "matched/matched_qsos.fits", overwrite=True)
     matched_ai.write(paths.data / "matched/matched_ai.fits", overwrite=True)
     bals.write(paths.data / "matched/matched_bals.fits", overwrite=True)
